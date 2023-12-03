0e302fdfb8bf4e9ec5c4f881c00cdac0664cbb14
seven <786852516@qq.com>
2023-12-03 17:25:50 +0800

    潘马

--- tenyear_ex.lua
+++ tenyear_ex.lua
@@ -2940,6 +2940,88 @@ Fk:loadTranslationTable{
   ["~ty_ex__liru"] = "多行不义，必自毙。",
 }
 
+local ty_ex__panzhangmazhong = General(extension, "ty_ex__panzhangmazhong", "wu", 4)
+local ty_ex__anjian = fk.CreateTriggerSkill{
+  name = "ty_ex__anjian",
+  anim_type = "offensive",
+  frequency = Skill.Compulsory,
+  events = {fk.TargetSpecified},
+  can_trigger = function(self, event, target, player, data)
+    return target == player and data.card.trueName == "slash" and not player.room:getPlayerById(data.to):inMyAttackRange(player)
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    data.additionalDamage = (data.additionalDamage or 0) + 1
+    room:addPlayerMark(room:getPlayerById(data.to), fk.MarkArmorNullified)
+    data.extra_data = data.extra_data or {}
+    data.extra_data.ty_ex__anjian = data.extra_data.ty_ex__anjian or {}
+    data.extra_data.ty_ex__anjian[tostring(data.to)] = (data.extra_data.ty_ex__anjian[tostring(data.to)] or 0) + 1
+  end,
+
+  refresh_events = {fk.CardUseFinished},
+  can_refresh = function(self, event, target, player, data)
+    return data.extra_data and data.extra_data.ty_ex__anjian
+  end,
+  on_refresh = function(self, event, target, player, data)
+    local room = player.room
+    for key, num in pairs(data.extra_data.ty_ex__anjian) do
+      local p = room:getPlayerById(tonumber(key))
+      if p:getMark(fk.MarkArmorNullified) > 0 then
+        room:removePlayerMark(p, fk.MarkArmorNullified, num)
+      end
+    end
+    data.extra_data.ty_ex__anjian = nil
+  end,
+}
+local ty_ex__anjian_delay = fk.CreateTriggerSkill{
+  name = "#ty_ex__anjian_delay",
+  mute = true,
+  frequency = Skill.Compulsory,
+  events = {fk.EnterDying},
+  can_trigger = function(self, event, target, player, data)
+    if player == target then
+      player.room.logic:dumpEventStack()
+      local e = player.room.logic:getCurrentEvent():findParent(GameEvent.CardEffect)
+      if e then
+        local use = e.data[1]
+        if use.extra_data and use.extra_data.ty_ex__anjian and use.extra_data.ty_ex__anjian[tostring(math.floor(target.id))] then
+          return true
+        end
+      end
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    data.extra_data = data.extra_data or {}
+    data.extra_data.ty_ex__anjian_poison = true
+  end,
+}
+ty_ex__anjian:addRelatedSkill(ty_ex__anjian_delay)
+local ty_ex__anjian_prohibit = fk.CreateProhibitSkill{
+  name = "#ty_ex__anjian_prohibit",
+  prohibit_use = function(self, player, card)
+    if card and card.name == "peach" and player.dying then
+      if RoomInstance and RoomInstance.logic:getCurrentEvent().event == GameEvent.Dying then
+        local data = RoomInstance.logic:getCurrentEvent().data[1]
+        return data and data.extra_data and data.extra_data.ty_ex__anjian_poison
+      end
+    end
+  end,
+}
+ty_ex__panzhangmazhong:addSkill("duodao")
+ty_ex__anjian:addRelatedSkill(ty_ex__anjian_prohibit)
+ty_ex__panzhangmazhong:addSkill(ty_ex__anjian)
+Fk:loadTranslationTable{
+  ["ty_ex__panzhangmazhong"] = "潘璋马忠",
+  ["ty_ex__anjian"] = "暗箭",
+  [":ty_ex__anjian"] = "锁定技，当你使用【杀】指定一名角色为目标后，若你不在其攻击范围内，此【杀】对其造成的基础伤害值+1且无视其防具，然后若该角色因此进入濒死状态，其不能使用【桃】直到此次濒死结算结束。",
+
+  ["$duodao_ty_ex__panzhangmazhong1"] = "宝刀配英雄，此刀志在必得！",
+  ["$duodao_ty_ex__panzhangmazhong2"] = "你根本不会用刀！",
+  ["$ty_ex__anjian1"] = "暗箭中人，其疮及骨！",
+  ["$ty_ex__anjian2"] = "战阵之间，不厌诈伪！",
+  ["~ty_ex__panzhangmazhong"] = "不知黄雀在其傍！",
+}
+
 --yj2014
 local chenqun = General(extension, "ty_ex__chenqun", "wei", 3)
 local ty_ex__pindi = fk.CreateActiveSkill{

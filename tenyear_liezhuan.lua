710494f0f108bc8be6bde9d6166580b2f1f911f8
seven <786852516@qq.com>
2024-05-12 22:07:54 +0800

    曹嵩

--- tenyear_liezhuan.lua
+++ tenyear_liezhuan.lua
@@ -1069,7 +1069,7 @@ local yizhengc = fk.CreateTriggerSkill{
     end
   end,
 
-  refresh_events = {fk.TurnStart},
+  refresh_events = {fk.TurnStart, fk.Death},
   can_refresh = function(self, event, target, player, data)
     return target == player and player:getMark(self.name) ~= 0
   end,

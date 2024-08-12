3c7b5b32c9eef37ac0b4405289e8e3dd08882292
Ho-spair <linyuy@163.com>
2024-08-12 22:52:04 +0800

    隐藏齐心标记

--- tenyear_other.lua
+++ tenyear_other.lua
@@ -2340,7 +2340,7 @@ local qixin = fk.CreateActiveSkill{
   prompt = "#qixin-active",
   switch_skill_name = "qixin",
   can_use = function(self, player)
-    return player:getMark("qixinUsed-phase") == 0 and player:getMark("@qixin_restore") > 0
+    return player:getMark("qixinUsed-phase") == 0 and player:getMark("qixin_restore") > 0
   end,
   card_filter = Util.FalseFunc,
   target_filter = function(self, to_select, selected)
@@ -2357,8 +2357,8 @@ local qixin = fk.CreateActiveSkill{
     room:setPlayerMark(player, "@!qixi_" .. (player.gender == General.Male and "male" or "female"), 1)
     room:setPlayerMark(player, "@!qixi_" .. (player.gender == General.Male and "female" or "male"), 0)
 
-    local hpRecord = player:getMark("@qixin_restore")
-    room:setPlayerMark(player, "@qixin_restore", player.hp)
+    local hpRecord = player:getMark("qixin_restore")
+    room:setPlayerMark(player, "qixin_restore", player.hp)
     room:changeHp(player, hpRecord - player.hp)
   end,
 }
@@ -2369,7 +2369,7 @@ local qixinTrigger = fk.CreateTriggerSkill{
   switch_skill_name = "qixin",
   events = {fk.AskForPeachesDone},
   can_trigger = function (self, event, target, player, data)
-    return target == player and player.dying and player:getMark("@qixin_restore") > 0
+    return target == player and player.dying and player:getMark("qixin_restore") > 0
   end,
   on_cost = Util.TrueFunc,
   on_use = function (self, event, target, player, data)
@@ -2383,8 +2383,8 @@ local qixinTrigger = fk.CreateTriggerSkill{
     room:setPlayerMark(player, "@!qixi_" .. (player.gender == General.Male and "male" or "female"), 1)
     room:setPlayerMark(player, "@!qixi_" .. (player.gender == General.Male and "female" or "male"), 0)
 
-    local hpRecord = player:getMark("@qixin_restore")
-    room:setPlayerMark(player, "@qixin_restore", 0)
+    local hpRecord = player:getMark("qixin_restore")
+    room:setPlayerMark(player, "qixin_restore", 0)
     room:changeHp(player, hpRecord - player.hp)
   end,
 
@@ -2397,16 +2397,16 @@ local qixinTrigger = fk.CreateTriggerSkill{
     return
       target == player and
       data == self and
-      not (event == fk.EventLoseSkill and player:getMark("@qixin_restore") == 0)
+      not (event == fk.EventLoseSkill and player:getMark("qixin_restore") == 0)
   end,
   on_refresh = function(self, event, target, player, data)
     local room = player.room
     if event == fk.EventAcquireSkill then
-      room:setPlayerMark(player, "@qixin_restore", player.maxHp)
+      room:setPlayerMark(player, "qixin_restore", player.maxHp)
       room:setPlayerProperty(player, "gender", General.Male)
       room:setPlayerMark(player, "@!qixi_male", 1)
     elseif event == fk.EventLoseSkill then
-      room:setPlayerMark(player, "@qixin_restore", 0)
+      room:setPlayerMark(player, "qixin_restore", 0)
     else
       room:setPlayerMark(player, "qixinUsed-phase", 0)
     end

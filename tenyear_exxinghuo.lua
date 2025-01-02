a7333fbf6d5096461f8dd72e77b91056235986f6
seven <786852516@qq.com>
2025-01-03 01:39:42 +0800

    修改 界马忠

--- tenyear_exxinghuo.lua
+++ tenyear_exxinghuo.lua
@@ -870,53 +870,74 @@ local fuman = fk.CreateActiveSkill{
     return #selected == 0 and not Self:prohibitDiscard(to_select)
   end,
   target_filter = function(self, to_select, selected)
-    return #selected == 0 and to_select ~= Self.id and not table.contains(Self:getTableMark("ty_ex__fuman-turn"), to_select)
+    return #selected == 0 and not table.contains(Self:getTableMark("ty_ex__fuman-phase"), to_select)
   end,
   on_use = function(self, room, effect)
     local player = room:getPlayerById(effect.from)
     local target = room:getPlayerById(effect.tos[1])
-    room:addTableMark(player, "ty_ex__fuman-turn", target.id)
+    room:addTableMark(player, "ty_ex__fuman-phase", target.id)
     room:throwCard(effect.cards, self.name, player, player)
     if target.dead then return end
     local card = room:getCardsFromPileByRule("slash", 1, "discardPile")
     if #card > 0 then
       room:addTableMark(target, self.name, {player.id, card[1]})
-      room:moveCardTo(card, Card.PlayerHand, target, fk.ReasonJustMove, self.name, nil, true, player.id, "@@ty_ex__fuman-inhand")
+      room:moveCardTo(card, Card.PlayerHand, target, fk.ReasonJustMove, self.name, nil, true, player.id, {"@@ty_ex__fuman", player.id})
     end
   end,
 }
 local fuman_delay = fk.CreateTriggerSkill{
   name = "#ty_ex__fuman_delay",
   anim_type = "drawcard",
-  events = {fk.CardUsing, fk.CardResponding},
+  events = {fk.AfterCardsMove},
   can_trigger = function(self, event, target, player, data)
-    return target == player and
-      table.find(player:getTableMark("ty_ex__fuman"), function (info)
-        return info[2] == data.card.id
-      end)
+    return not player.dead and data.extra_data and data.extra_data.ty_ex__fuman_draw
+    and data.extra_data.ty_ex__fuman_draw[player.id]
   end,
   on_cost = Util.TrueFunc,
   on_use = function (self, event, target, player, data)
-    local room = player.room
-    for _, info in ipairs(player:getTableMark("ty_ex__fuman")) do
-      if info[2] == data.card.id then
-        local p = room:getPlayerById(info[1])
-        if not p.dead then
-          p:drawCards(1, "ty_ex__fuman")
-        end
-        if not player.dead then
-          player:drawCards(1, self.name)
+    player:drawCards(data.extra_data.ty_ex__fuman_draw[player.id], self.name)
+  end,
+
+  refresh_events = {fk.AfterTurnEnd, fk.AfterCardsMove},
+  can_refresh = function (self, event, target, player, data)
+    if event == fk.AfterTurnEnd then return target == player end
+    if player.seat ~= 1 then return false end
+    for _, move in ipairs(data) do
+      if move.from then
+        for _, info in ipairs(move.moveInfo) do
+          if info.fromArea == Card.PlayerHand and Fk:getCardById(info.cardId):getMark("@@ty_ex__fuman") ~= 0 then
+            return true
+          end
         end
       end
     end
   end,
-
-  refresh_events = {fk.TurnEnd},
-  can_refresh = function (self, event, target, player, data)
-    return target == player and player:getMark("ty_ex__fuman") ~= 0
-  end,
   on_refresh = function (self, event, target, player, data)
-    player.room:setPlayerMark(player, "ty_ex__fuman", 0)
+    local room = player.room
+    if event == fk.AfterTurnEnd then
+      for _, id in ipairs(player:getCardIds("h")) do
+        if Fk:getCardById(id):getMark("@@ty_ex__fuman") ~= 0 then
+          room:setCardMark(Fk:getCardById(id), "@@ty_ex__fuman", 0)
+        end
+      end
+    else
+      data.extra_data = data.extra_data or {}
+      data.extra_data.ty_ex__fuman_draw = data.extra_data.ty_ex__fuman_draw or {}
+      for _, move in ipairs(data) do
+        if move.from then
+          for _, info in ipairs(move.moveInfo) do
+            local owner = Fk:getCardById(info.cardId):getMark("@@ty_ex__fuman")
+            if info.fromArea == Card.PlayerHand and owner ~= 0 then
+              room:setCardMark(Fk:getCardById(info.cardId), "@@ty_ex__fuman", 0)
+              data.extra_data.ty_ex__fuman_draw[move.from] = (data.extra_data.ty_ex__fuman_draw[move.from] or 0) + 1
+              if move.moveReason == fk.ReasonUse or move.moveReason == fk.ReasonResonpse then
+                data.extra_data.ty_ex__fuman_draw[owner] = (data.extra_data.ty_ex__fuman_draw[owner] or 0) + 1
+              end
+            end
+          end
+        end
+      end
+    end
   end,
 }
 fuman:addRelatedSkill(fuman_delay)
@@ -927,9 +948,8 @@ Fk:loadTranslationTable{
   ["illustrator:ty_ex__mazhong"] = "君桓文化",
 
   ["ty_ex__fuman"] = "抚蛮",
-  [":ty_ex__fuman"] = "出牌阶段每名角色限一次，你可以弃置一张牌，令一名其他角色随机获得弃牌堆一张【杀】。若如此做，直到其下回合结束，"..
-  "当其使用或打出此【杀】时，你与其各摸一张牌。",
-  ["#ty_ex__fuman"] = "抚蛮：弃一张牌，令一名角色获得一张【杀】，其使用或打出此【杀】时你与其各摸一张牌",
+  [":ty_ex__fuman"] = "出牌阶段每名角色限一次，你可以弃置一张牌，令一名角色从弃牌堆中获得一张【杀】，然后其于其回合结束前失去此【杀】时，其摸一张牌。若为因使用或打出而失去，你摸一张牌。",
+  ["#ty_ex__fuman"] = "抚蛮：弃一张牌，令一名角色获得一张【杀】，其于其下个回合结束前失去时摸一张牌",
   ["@@ty_ex__fuman"] = "抚蛮",
   ["#ty_ex__fuman_delay"] = "抚蛮",
 

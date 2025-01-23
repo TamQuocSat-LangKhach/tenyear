da91c8169a7c7139e68a3afb0882c0f153ed68cb
xxyheaven <1433191064@qq.com>
2025-01-23 20:57:07 +0800

    彩翼desc

--- tenyear_sp2.lua
+++ tenyear_sp2.lua
@@ -5809,10 +5809,61 @@ local function doCaiyi(player, target, choice, n)
     end
   end
 end
+
 local caiyi = fk.CreateTriggerSkill{
   name = "caiyi",
   anim_type = "switch",
   switch_skill_name = "caiyi",
+  dynamic_desc = function(self, player)
+    if table.every({1,2,3,4}, function(i)
+      return player:getMark("caiyiyang" .. i) > 0 and player:getMark("caiyiyinn" .. i) > 0
+    end) then
+      return "dummyskill"
+    end
+    local highlight_color = "<font color='#E0DB2F'>"
+    local gray_color = "<font color='gray'>"
+    local clean_color = "</font>"
+    local texts = {"caiyi_inner"}
+    local isYang = player:getSwitchSkillState(self.name, false) == fk.SwitchYang
+    local colors = {clean_color, gray_color}
+    if isYang then
+      table.insert(texts, highlight_color)
+      colors = {clean_color .. highlight_color, clean_color .. gray_color}
+    else
+      table.insert(texts, "")
+    end
+    local x = 1
+    for i = 1, 4, 1 do
+      if player:getMark("caiyiyang" .. i) == x then
+        table.insert(texts, colors[x+1])
+        x = 1-x
+      else
+        table.insert(texts, "")
+      end
+    end
+    if not isYang then
+      if x == 0 then
+        table.insert(texts, clean_color .. highlight_color)
+      else
+        table.insert(texts, highlight_color)
+      end
+      colors = {clean_color .. highlight_color, clean_color .. gray_color}
+    else
+      table.insert(texts, clean_color)
+      colors = {clean_color, gray_color}
+    end
+    x = 1
+    for i = 1, 4, 1 do
+      if player:getMark("caiyiyinn" .. i) == x then
+        table.insert(texts, colors[x+1])
+        x = 1-x
+      else
+        table.insert(texts, "")
+      end
+    end
+    table.insert(texts, clean_color)
+    return table.concat(texts, ":")
+  end,
   events = {fk.EventPhaseStart},
   can_trigger = function(self, event, target, player, data)
     if target == player and player:hasSkill(self) and player.phase == Player.Finish then
@@ -5949,7 +6000,7 @@ Fk:loadTranslationTable{
   ["illustrator:caohua"] = "HEI-LE",
   ["caiyi"] = "彩翼",
   [":caiyi"] = "转换技，结束阶段，你可以令一名角色选择一项并移除该选项：阳：1.回复X点体力；2.摸X张牌；3.复原武将牌；4.随机执行一个已移除的阳选项；"..
-  "阴：1.受到X点伤害；2.弃置X张牌；3.翻面并横置；4.随机执行一个已移除的阴选项（X为当前状态剩余选项数）。",
+  "阴：1.受到X点伤害；2.弃置X张牌；3.翻面并横置；4.随机执行一个已移除的阴选项。（X为当前状态剩余选项数）",
   ["guili"] = "归离",
   [":guili"] = "你的第一个回合开始时，你选择一名其他角色。该角色每轮的第一个回合结束时，若其本回合未造成过伤害，你执行一个额外的回合。",
   ["#caiyi1-invoke"] = "彩翼：你可以令一名角色执行一个正面选项",
@@ -5965,6 +6016,10 @@ Fk:loadTranslationTable{
   ["caiyiyinn4"] = "随机一个已移除的阴选项",
   ["@@guili"] = "归离",
   ["#guili-choose"] = "归离：选择一名角色，其回合结束时，若其本回合未造成过伤害，你执行一个额外回合",
+
+  [":caiyi_inner"] = "转换技，结束阶段，你可以令一名角色选择一项并移除该选项："..
+  "{1}阳：{2}1.回复X点体力；{3}2.摸X张牌；{4}3.复原武将牌；{5}4.随机执行一个已移除的阳选项；"..
+  "{6}阴：{7}1.受到X点伤害；{8}2.弃置X张牌；{9}3.翻面并横置；{10}4.随机执行一个已移除的阴选项{11}。（X为当前状态剩余选项数）",
 
   ["$caiyi1"] = "凰凤化越，彩翼犹存。",
   ["$caiyi2"] = "身披彩翼，心有灵犀。",
